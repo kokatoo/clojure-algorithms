cb3db49049d6a7ca18a5bd2dd54ed36d47674ab3
Jason Kok <kokatoo1948@gmail.com>
2014-12-07 15:35:38 +0800

    Use split-with instead of 2 filter statements.

--- src/clojure_algorithms/sort.clj
+++ src/clojure_algorithms/sort.clj
@@ -3,8 +3,8 @@
 (defn insertion-sort [coll]
   "insertion sort"
   (letfn [(insert [elm coll]
-                  (into []
-                   (concat (filter #(<= % elm) coll) [elm] (filter #(> % elm) coll))))]
+                  (let [[left right] (split-with (partial >= elm) coll)]
+                   (reduce concat [left [elm] right])))]
     (loop [left []
            right coll]
       (if (empty? right)
