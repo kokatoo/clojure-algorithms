5b368bf94b3845af2e65c29f8f9a383907ff41be
Jason Kok <kokatoo1948@gmail.com>
2014-12-07 18:07:51 +0800

    Add merge-sort-iter that doesn't stackoverflow.

--- src/clojure_algorithms/sort.clj
+++ src/clojure_algorithms/sort.clj
@@ -26,7 +26,27 @@
       coll
       (let [[left right] (split-at (/ (count coll) 2) coll)]
         (*merge (merge-sort left) (merge-sort right))))))
+      
+(defn merge-sort-iter [coll]
+  (let [[elm & *coll] coll]
+    (if (nil? *coll)
+      coll
+      (let [[left right] (split-at (/ (count coll) 2) coll)]
+        (*merge-iter (trampoline #(merge-sort-iter left))
+                     (trampoline #(merge-sort-iter right)))))))
 
-
-
-
+(defn *merge-iter [left right]
+  (letfn [(**merge [left right result]
+                   (let [[l1 & *left] left
+                         [r1 & *right] right]
+                     (cond
+                      (and (nil? l1) (nil? r1)) result
+                      (and (some? l1) (some? r1))
+                        (if (<= l1 r1)
+                          (recur *left right (conj result l1))
+                          (recur left *right (conj result r1)))
+                       (nil? l1)
+                         (recur left *right (conj result r1))
+                       (nil? r1)
+                         (recur *left right (conj result l1)))))]
+    (**merge left right [])))
