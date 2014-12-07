1a85541503645b806be5164100b25ebfcb0d11a9
Jason Kok <kokatoo1948@gmail.com>
2014-12-07 16:33:36 +0800

    Add merge-sort.

--- src/clojure_algorithms/sort.clj
+++ src/clojure_algorithms/sort.clj
@@ -10,8 +10,22 @@
       (if (empty? right)
         left
         (recur (insert (first right) left) (rest right))))))
-
-
+         
+(defn *merge [left right]
+  (cond (nil? left) right
+        (nil? right) left
+        :else (let [[l1 & *left] left
+                    [r1 & *right] right]
+                (if (<= l1 r1)
+                  (cons l1 (*merge *left right))
+                  (cons r1 (*merge left *right))))))
+                
+(defn merge-sort [coll]
+  (let [[elm & *coll] coll]
+    (if (nil? *coll)
+      coll
+      (let [[left right] (split-at (/ (count coll) 2) coll)]
+        (*merge (merge-sort left) (merge-sort right))))))
 
 
 
