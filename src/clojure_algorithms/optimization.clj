1a2edafedbb0cab9beaf56d86cc2d6d3aead16f4
Jason Kok <kokatoo1948@gmail.com>
2014-12-09 12:58:45 +0800

    Add SubArray record.

--- src/clojure_algorithms/optimization.clj
+++ src/clojure_algorithms/optimization.clj
@@ -1,37 +1,45 @@
 (ns clojure-algorithms.optimization)
 
-(defn max-idx-sum [coll offset-fn]
+(defrecord SubArray [low high sum])
+
+(defn max-idx-sum
+  "Calculate the left/right index of the max sum"
+  [coll offset-fn]
     (let [sums (map #(apply + %) coll)
           max-sum (apply max sums)
           idx (.indexOf sums (apply max sums))]
       [(offset-fn idx), max-sum]))
 
 (defn max-cross-subarray [coll low mid high]
+  "Max sum of subarray given mid-point"
   (let [lefts (subvec coll low (inc mid))
         rights (subvec coll (inc mid) (inc high))
         [left-idx, left-sum] (max-idx-sum (take (count lefts) (iterate rest lefts)) (partial + low))
         [right-idx, right-sum] (max-idx-sum (take (count rights) (iterate drop-last rights)) (partial - high))]
-    [left-idx, right-idx, (+ left-sum right-sum)]))
+    (SubArray. left-idx right-idx (+ left-sum right-sum))))
 
 ;(take 3 (iterate rest [20 18 -23]))
 ;(take 3 (iterate drop-last [-23 18 20]))
 ;(max-cross-subarray [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7] 3 5 8)
 
 (defn max-subarray
+  "Max SubArray using divide and conquer O(n log(n))"
   ([coll] (max-subarray coll 0 (dec (count coll))))
   ([coll low high]
    (if (= low high)
-     [low, high, (nth coll low)]
+     (SubArray. low high (nth coll low))
      (let [mid (int (/ (+ low high) 2))
-           [left-low, left-high, left-sum] (max-subarray coll, low, mid)
-           [right-low, right-high, right-sum] (max-subarray coll, (inc mid), high)
-           [cross-low, cross-high, cross-sum] (max-cross-subarray coll, low, mid, high)
-           max-sum (max left-sum right-sum cross-sum)]
-       (cond (= max-sum left-sum) [left-low, left-high, left-sum]
-             (= max-sum right-sum) [right-low, right-high, right-sum]
-             :else [cross-low, cross-high, cross-sum])))))
+           left (max-subarray coll, low, mid)
+           right (max-subarray coll, (inc mid), high)
+           cross (max-cross-subarray coll, low, mid, high)
+           max-sum (max (:sum left) (:sum right) (:sum cross))]
+       (println left "," right "," cross "," max-sum)
+       (cond (= max-sum (:sum left)) left
+             (= max-sum (:sum right)) right
+             :else cross)))))
 
-
-(max-subarray [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]); 7 10 43
-(max-subarray [-2 -5 6 -2 -3 1 5 -6]); 2 6 7
+;(= (max-subarray [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7])
+;   (SubArray. 7 10 43))
+;(max-subarray [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]); 7 10 43
+;(max-subarray [-2 -5 6 -2 -3 1 5 -6]); 2 6 7
 (max-subarray [-2 4])
