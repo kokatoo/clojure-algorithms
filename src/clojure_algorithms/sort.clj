06e805c437acbd29c713c48a9e8e4bd2a7066260
jason.kok <kokatoo1948@gmail.com>
2014-12-21 16:52:32 +0800

    Adhere more to coding guidelines

--- src/clojure_algorithms/sort.clj
+++ src/clojure_algorithms/sort.clj
@@ -1,18 +1,23 @@
 (ns clojure-algorithms.sort)
 
-(defn insert-sort [coll]
+(defn insert-sort
   "Insertion Sort"
-  (letfn [(insert [elm coll]
-                  (into []
-                   (concat (filter #(<= % elm) coll) [elm] (filter #(> % elm) coll))))]
+  [coll]
+  (letfn [(insert
+           [elm coll]
+           (into []
+                 (concat (filter #(<= % elm) coll)
+                         [elm]
+                         (filter #(> % elm) coll))))]
     (loop [left []
            right coll]
       (if (empty? right)
         left
         (recur (insert (first right) left) (rest right))))))
 
-(defn *merge [left right]
+(defn *merge
   "Merge two sorted collections into sorted collection"
+  [left right]
   (lazy-seq
    (cond (nil? left) right
          (nil? right) left
@@ -22,21 +27,23 @@
                    (cons l1 (*merge *left right))
                    (cons r1 (*merge left *right)))))))
 
-(defn *merge-iter [left right]
+(defn *merge-iter
   "Merge two sorted collections into sorted collection (iteration)"
-  (letfn [(**merge [left right result]
-                   (let [[l1 & *left] left
-                         [r1 & *right] right]
-                     (cond
-                      (and (nil? l1) (nil? r1)) result
-                      (and (some? l1) (some? r1))
-                        (if (<= l1 r1)
-                          (recur *left right (conj result l1))
-                          (recur left *right (conj result r1)))
-                      (nil? l1)
-                        (recur left *right (conj result r1))
-                      (nil? r1)
-                        (recur *left right (conj result l1)))))]
+  [left right]
+  (letfn [(**merge
+           [left right result]
+           (let [[l1 & *left] left
+                 [r1 & *right] right]
+             (cond
+              (and (nil? l1) (nil? r1)) result
+              (and (some? l1) (some? r1))
+              (if (<= l1 r1)
+                (recur *left right (conj result l1))
+                (recur left *right (conj result r1)))
+              (nil? l1)
+              (recur left *right (conj result r1))
+              (nil? r1)
+              (recur *left right (conj result l1)))))]
     (**merge left right [])))
 
 (defn merge-sort-wrapper
@@ -48,18 +55,22 @@
       (let [[left right] (split-at (/ (count coll) 2) coll)]
         (*merge-sort left right)))))
 
-(defn merge-sort [coll]
+(defn merge-sort
   "Merge Sort (Recursive)"
-  (merge-sort-wrapper coll
-                      (fn [left right]
-                        (*merge (merge-sort left) (merge-sort right)))))
+  [coll]
+  (merge-sort-wrapper
+   coll
+   (fn [left right]
+     (*merge (merge-sort left) (merge-sort right)))))
 
-(defn merge-sort-iter [coll]
+(defn merge-sort-iter
   "Merge Sort (Iterative)"
-  (merge-sort-wrapper coll
-                      (fn [left right]
-                        (*merge-iter (trampoline #(merge-sort-iter left))
-                                     (trampoline #(merge-sort-iter right))))))
+  [coll]
+  (merge-sort-wrapper
+   coll
+   (fn [left right]
+     (*merge-iter (trampoline #(merge-sort-iter left))
+                  (trampoline #(merge-sort-iter right))))))
 
 ;; HeapSort
 (defn- swap [coll i j]
@@ -67,14 +78,16 @@
 (defn- in? [elm coll]
   (some #(= elm %) coll))
 
-(defn- max-heapify [coll start end]
+(defn- max-heapify
+  [coll start end]
   (loop [coll coll x start left (inc (* 2 x))]
     (let [right (inc left)]
       (if (>= left end)
         coll
-        (let [child (if (and (< left (dec end)) (< (nth coll left) (nth coll right)))
-                   right
-                   left)]
+        (let [child
+              (if (and (< left (dec end)) (< (nth coll left) (nth coll right)))
+                right
+                left)]
           (if (< (nth coll x) (nth coll child))
             (recur (swap coll x child) child (inc (* 2 child)))
             coll))))))
@@ -82,15 +95,21 @@
 (defn- build-max-heap
   ([coll] (build-max-heap coll (count coll)))
   ([coll len]
-    (reduce #(max-heapify %1 %2 len) (vec coll) (range (dec (int (/ len 2))) -1 -1))))
+    (reduce #(max-heapify %1 %2 len)
+            (vec coll)
+            (range (dec (int (/ len 2))) -1 -1))))
 
-(defn heapsort [coll]
+(defn heapsort
   "Heap Sort"
-  (reduce #(max-heapify (swap %1 %2 0) 0 %2)(build-max-heap coll)(range (dec (count coll)) 0 -1)))
+  [coll]
+  (reduce #(max-heapify (swap %1 %2 0) 0 %2)
+          (build-max-heap coll)
+          (range (dec (count coll)) 0 -1)))
 
 ;; QuickSort
-(defn quicksort [[pivot & xs :as coll]]
+(defn quicksort
   "Quick Sort"
+  [[pivot & xs :as coll]]
   (if pivot
     (let [smaller (filter #(< % pivot) xs)
           bigger (filter #(> % pivot) xs)
@@ -100,8 +119,9 @@
                  (quicksort bigger)))
     []))
 
-(defn rand-quicksort [coll]
+(defn rand-quicksort
   "Rand Pivot QuickSort"
+  [coll]
   (if (empty? coll)
     []
     (let [pivot (nth coll (rand-int (count coll)))
@@ -114,14 +134,17 @@
                 (rand-quicksort bigger)))))
 
 ;; Counting Sort
-(defn- counts-less-eq [coll n-buckets *get-elm]
+(defn- counts-less-eq
   "Returns a vector of counts <= to index"
-  (let [len (count coll)
-        counts-eq (reduce (fn [acc idx]
-                            (let [elm (*get-elm coll idx)]
-                              (assoc acc elm (inc (acc elm)))))
-                          (-> (replicate n-buckets 0) vec)
-                          (range len))]
+  [coll n-buckets *get-elm]
+  (let [len
+        (count coll)
+        counts-eq
+        (reduce (fn [acc idx]
+                  (let [elm (*get-elm coll idx)]
+                    (assoc acc elm (inc (acc elm)))))
+                (-> (replicate n-buckets 0) vec)
+                (range len))]
     (reduce (fn [acc idx]
               (assoc acc idx (+ (acc idx) (acc (dec idx)))))
             counts-eq
@@ -129,10 +152,12 @@
 
 (defn countingsort
   "Counting Sort"
-  ([coll n-buckets] (countingsort coll
-                                  n-buckets
-                                  (fn [coll idx]
-                                    (nth coll idx))))
+  ([coll n-buckets]
+   (countingsort
+    coll
+    n-buckets
+    (fn [coll idx]
+      (nth coll idx))))
   ([coll n-buckets *get-elm]
    (let [len (count coll)
          coll (vec coll)]
@@ -146,23 +171,25 @@
                 (dec idx)))))))
 
 ;; Radix Sort
-(defn- ret-pos [num base pos]
+(defn- ret-pos
   "Returns the digit by pos"
+  [num base pos]
   (let [exp (Math/pow base pos)]
     (-> (/ num exp)
         (mod base)
         int)))
 
-(defn radixsort [coll ndigits]
+(defn radixsort
   "Radix Sort"
+  [coll ndigits]
   (let [len (count coll)
         coll (vec coll)]
     (loop [i 0 result coll]
       (if (= i ndigits)
         result
         (recur (inc i)
-               (countingsort result
-                             10
-                             (fn [coll idx]
-                               (ret-pos (nth coll idx) 10 i))))))))
-
+               (countingsort
+                result
+                10
+                (fn [coll idx]
+                  (ret-pos (nth coll idx) 10 i))))))))
